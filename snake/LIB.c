4082c6f278b6df15f53d34f6579b714ae529ef9f
John Henry <30395884+johnphenry@users.noreply.github.com>
2017-09-12 02:14:20 -0400

    Update LIB.c

--- snake/LIB.c
+++ snake/LIB.c
@@ -1,3 +1,12 @@
+/*
+When porting this game to other platforms, this is the only code that needs to be modified if the platform is not supported by SDL2.
+Note that headers for libraries such as SDL2 are not incuded in other headers, only here in this file.
+This speeds up compilation times by a lot because headers are only included where they are needed.
+The actual implementation of the functions in this file does not matter to any of the other files in the game.
+This forces that only the dependent code can be used here, and nowhere else.
+I consider even the usage of that standard library to be dependent code.
+For instance, when using libGCCVB, a virtual boy compiler, the standard library actually has not been implemented for that platform!
+*/
 #include<stdlib.h>
 #include<time.h>
 #include<SDL2/SDL.h>
